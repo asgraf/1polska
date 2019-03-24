3b9f6c0c232a66a04512d41f053c065afdd8b9e4
Michal Asgraf Turek <asgraf@gmail.com>
2019-03-24 19:43:42 +0100

    aktualizacja do nowego API youtube'a

--- webroot/js/apply_markdown.js
+++ webroot/js/apply_markdown.js
@@ -3,20 +3,20 @@ $(function(){
 
 	function getYoutubeId(url){
 		if(!url) return false;
-		var match = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
+		const match = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/);
 		return match&&match[7].length==11?match[7]:false;
 	}
 
 	if(typeof markdown == 'object') {
-		var mobile_browser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+		const mobile_browser = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 		$('[data-markdown]').each(function(idx,el){
-			var $el = $(el);
-			var content = $el.text();
+			const $el = $(el);
+			let content = $el.text();
 			content = markdown.toHTML(content);
 			$el.html(content);
 			if(!mobile_browser) {
 				$el.find('a').each(function(aidx,a){
-					var youtube_id = getYoutubeId(a.href);
+					const youtube_id = getYoutubeId(a.href);
 					if(youtube_id) {
 						$(a).replaceWith('<iframe width="640" height="360" src="https://www.youtube.com/embed/'+youtube_id+'" frameborder="0" allowfullscreen></iframe>');
 					}
@@ -32,7 +32,7 @@ $(function(){
 						title: 'Youtube',
 						icon: 'youtube-icon',
 						callback: function(e){
-							var chunk, cursor, selected = e.getSelection(),youtube_id,youtube_data;
+							let chunk, cursor, selected = e.getSelection(),youtube_id,youtube_data;
 							youtube_id = getYoutubeId(selected.text);
 							if(!youtube_id) {
 								youtube_id = getYoutubeId(prompt('Podaj link do filmiku na youtube'));
@@ -41,8 +41,12 @@ $(function(){
 								alert('To nie jest prawidłowy link do filmiku na youtube');
 								return;
 							}
-							youtube_data = $.ajax({url:'https://gdata.youtube.com/feeds/api/videos/'+youtube_id+'?v=2&alt=json',async:false,dataType:'json'}).responseJSON;
-							var alt = youtube_data.entry.title.$t;
+							youtube_data = $.ajax({url:'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&hl=pl&key=AIzaSyCm3JRvbk3XXRUh8t1VRCYlLYuqSCRUnBY&id='+youtube_id,async:false,dataType:'json'}).responseJSON;
+                            let alt = '';
+                            if(youtube_data && youtube_data.items && youtube_data.items[0] && youtube_data.items[0].snippet) {
+                                alt = youtube_data.items[0].snippet.title;
+                            }
+
 							chunk = '[!['+alt+'](https://img.youtube.com/vi/'+youtube_id+'/0.jpg)]( https://youtube.com/watch?v='+youtube_id+' "'+alt+'")';
 
 							e.replaceSelection(chunk);
